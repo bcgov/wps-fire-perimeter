a22441c583da8d8c89c4a9f8599cb1c838aaeea0
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-06-01 16:04:52 -0700

    functions

---
+++ query_by_number_and_date.sql
@@ -0,0 +1,22 @@
+create schema if not exists postgisftw;
+
+CREATE OR REPLACE FUNCTION postgisftw.fire_by_number_and_date(
+	fire_number text,
+	date_of_interest date)
+RETURNS TABLE(id integer, geom geometry, date_range integer, cloud_cover double precision,
+			 latitude double precision, longitude double precision,
+			 rgb_raster character varying, create_date timestamp with time zone, update_date timestamp with time zone)
+AS $$
+BEGIN
+	RETURN QUERY
+		SELECT t.id, t.geom, t.date_range, t.cloud_cover,
+		t.latitude, t.longitude,
+		t.rgb_raster, t.create_date, t.update_date
+    FROM public.featureserv t
+    WHERE t.fire_number LIKE fire_by_number_and_date.fire_number AND t.date_of_interest = fire_by_number_and_date.date_of_interest;
+END;
+$$
+
+LANGUAGE 'plpgsql' STABLE PARALLEL SAFE;
+
+COMMENT ON FUNCTION postgisftw.fire_by_number_and_date IS 'Filters the featureserv table by fire_number and date';
