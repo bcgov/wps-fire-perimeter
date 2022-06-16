a78c2adbeb53928d99b322080b7e9ea3f2f63a11
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-06-16 15:33:25 -0700

    update

--- fire_perimeter/cli.py
+++ fire_perimeter/cli.py
@@ -122,7 +122,7 @@ if __name__ == '__main__':
     dl_path = 'https://pub.data.gov.bc.ca/datasets/2790e3f7-6395-4230-8545-04efb5a18800/' + fn
     urllib.request.urlretrieve(dl_path, fn)
     
-    t = datetime.datetime.now().strftime("%Y%m%d%H%M%S")  # timestamped backup
+    t = datetime.datetime.now().strftime("%Y%m%d") #%H%M%S")  # timestamped backup
     shutil.copyfile(fn, 'prot_current_fire_points_' + t + '.zip')
     zipfile.ZipFile(fn).extractall()   
 
